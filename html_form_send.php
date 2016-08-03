fdc9220ba1f431de892d1743559137880679694e
Neil Mudjer <neil.mudjer@gmail.com>
2016-08-03 18:23:50 -0500

    s

--- html_form_send.php
+++ html_form_send.php
@@ -2,9 +2,9 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "you@yourdomain.com";
+    $email_to = "neil.mudjer@gmail.com";
      
-    $email_subject = "website html form submissions";
+    $email_subject = "Portfolio Email";
      
      
     function died($error) {
